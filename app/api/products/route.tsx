8875b038cf7060772d6f5c60994d37c2c082a835
Valentin715 <justvalk@yandex.ru>
2023-10-18 12:37:03 +0300

    Commit 5 Setup Prisma

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -3,15 +3,29 @@ import schema from "./schema";
 
 export function GET(request: NextRequest) {
   return NextResponse.json([
-    { id: 1, name: "Val" },
-    { id: 2, name: "John" },
+    {
+      id: 1,
+      name: "Milk",
+      price: 2.5,
+    },
+    {
+      id: 2,
+      name: "Bread",
+      price: 3.5,
+    },
   ]);
 }
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
+
   const validation = schema.safeParse(body);
+
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
-  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+
+  return NextResponse.json(
+    { id: 10, name: body.name, price: body.price },
+    { status: 201 }
+  );
 }
